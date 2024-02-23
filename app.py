405412ffdd4345c6f54daa3eea232e7833c9c088
Chris Gillespie <65926061+iamchrsgllsp@users.noreply.github.com>
2024-02-23 11:41:42 +0000

    some style

--- app.py
+++ app.py
@@ -10,9 +10,29 @@ def hello_world():
 
 @app.route('/test')
 def test():
-  return "test"
+  return """<form hx-put="/contact/1" hx-target="this" hx-swap="outerHTML">
+  <div>
+    <label>First Name</label>
+    <input type="text" name="firstName" value="Joe">
+  </div>
+  <div class="form-group">
+    <label>Last Name</label>
+    <input type="text" name="lastName" value="Blow">
+  </div>
+  <div class="form-group">
+    <label>Email Address</label>
+    <input type="email" name="email" value="joe@blow.com">
+  </div>
+  <button class="btn">Submit</button>
+  <button class="btn" hx-get="/contact/1">Cancel</button>
+</form>
+  """
 
 
 @app.route('/func')
 def func():
   return "func"
+
+
+#comment out below before deploying on render
+#app.run(host="0.0.0.0", port=8080, debug=True)
